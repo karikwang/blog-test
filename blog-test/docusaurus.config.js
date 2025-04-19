486c02cf64c737209221372f794e77e327f7db2e
Karik <wangmd1822@gmail.com>
2025-04-19 10:01:09 +0800

    Initial commit

---
+++ blog-test/docusaurus.config.js
@@ -0,0 +1,120 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+const { themes } = require("prism-react-renderer");
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "你的博客名称",
+  tagline: "你的博客标语",
+  favicon: "img/favicon.ico",
+
+  // Set the production url of your site here
+  url: "https://你的GitHub用户名.github.io",
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: "/blog-test/",
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "你的GitHub用户名", // Usually your GitHub org/user name.
+  projectName: "blog-test", // Usually your repo name.
+
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "zh-Hans",
+    locales: ["zh-Hans"],
+  },
+
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl: "https://github.com/你的GitHub用户名/blog-test/tree/main/",
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl: "https://github.com/你的GitHub用户名/blog-test/tree/main/",
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: "img/docusaurus-social-card.jpg",
+      navbar: {
+        title: "你的博客名称",
+        logo: {
+          alt: "My Site Logo",
+          src: "img/logo.svg",
+        },
+        items: [
+          {
+            type: "docSidebar",
+            sidebarId: "tutorialSidebar",
+            position: "left",
+            label: "文档",
+          },
+          { to: "/blog", label: "博客", position: "left" },
+          {
+            href: "https://github.com/你的GitHub用户名/blog-test",
+            label: "GitHub",
+            position: "right",
+          },
+        ],
+      },
+      footer: {
+        style: "dark",
+        links: [
+          {
+            title: "文档",
+            items: [
+              {
+                label: "文档",
+                to: "/docs/intro",
+              },
+            ],
+          },
+          {
+            title: "社区",
+            items: [
+              {
+                label: "GitHub",
+                href: "https://github.com/你的GitHub用户名/blog-test",
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} 你的名字. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: themes.github,
+        darkTheme: themes.dracula,
+      },
+    }),
+};
+
+export default config;
